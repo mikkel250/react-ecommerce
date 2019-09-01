69f89181934f556a4bb1a40116f30071e910fe35
Mikkel Ridley <mikkel250@gmail.com>
2019-09-01 00:40:41 -0700

    add cart dropdoooooooooowwwwwwwwwwwwnnnnnnnnnnn  toggle. ¯\_(ツ)_/¯

--- clothing/src/components/header/header.component.jsx
+++ clothing/src/components/header/header.component.jsx
@@ -2,12 +2,16 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { auth } from '../../firebase/firebase.utils';
 import { connect } from 'react-redux';
+
+import CartIcon from '../cart-icon/cart-icon.component';
+import CartDropdown from '../cart-dropdown/cart-dropdown.component';
+
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 
 import './header.styles.scss';
 
 
-const Header = ({currentUser}) => (
+const Header = ({currentUser, hidden}) => (
   <div className="header">
     <Link className="logo-container" to='/'>
       <Logo className="logo"></Logo>
@@ -22,14 +26,17 @@ const Header = ({currentUser}) => (
       {
         currentUser ? ( <div className='option' onClick={() => auth.signOut()} >SIGN OUT</div> ) : ( <Link className='option' to='/signin'> SIGN IN </Link> )
       }
+      <CartIcon />
     </div>
+    {hidden ? null : <CartDropdown />}
   </div>
 );
 
 // mapStateToProps and connect are used any time we want properties from the reducers 
-const mapStateToProps = state => ({ // state here is being pulled by redux from the root reducer
+const mapStateToProps = ({user: {currentUser}, cart: {hidden}}) => ({ // state here is being pulled by redux from the root reducer
   // property   value 
-  currentUser: state.user.currentUser
+  currentUser,
+  hidden
 });
 
 // connect returns a higher order component that takes our component (Header) and that returns a new "souped up" component that has access to the properties in the reducers
