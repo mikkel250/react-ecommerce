0992c17965b16b6e422424934c40912df80e071f
Mikkel Ridley <mikkel250@gmail.com>
2019-09-03 23:46:17 -0700

    add toggle cart hidden

--- clothing/src/components/cart-icon/cart-icon.component.jsx
+++ clothing/src/components/cart-icon/cart-icon.component.jsx
@@ -1,9 +1,5 @@
 import React from 'react';
 import { connect } from 'react-redux';
-<<<<<<< HEAD
-
-=======
->>>>>>> 69f89181934f556a4bb1a40116f30071e910fe35
 import { toggleCartHidden } from '../../redux/cart/cart.actions';
 
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
@@ -11,15 +7,9 @@ import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 import './cart-icon.styles.scss';
 
 const CartIcon = ({toggleCartHidden}) => (
-<<<<<<< HEAD
-  <div className="cart-icon" onClick={toggleCartHidden}>
-    <ShoppingIcon className='shopping-icon' />
-    <span className="item-count"></span>
-=======
   <div className="cart-icon"onClick={toggleCartHidden}>
     <ShoppingIcon className='shopping-icon' />
     <span className="item-count">0</span>
->>>>>>> 69f89181934f556a4bb1a40116f30071e910fe35
   </div>
 );
 
