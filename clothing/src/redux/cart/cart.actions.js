f2c04e55e082eb8f052023b33058b5f302668e80
Mikkel Ridley <mikkel250@gmail.com>
2019-09-03 23:47:47 -0700

    add cart.action

--- clothing/src/redux/cart/cart.actions.js
+++ clothing/src/redux/cart/cart.actions.js
@@ -1,10 +1,5 @@
 import CartActionTypes from './cart.types';
 
 export const toggleCartHidden = () => ({
-<<<<<<< HEAD
   type: CartActionTypes.TOGGLE_CART_HIDDEN
 });
-=======
-  type: CartActionTypes.TOGGLE_CART_HIDDEN      
-});
->>>>>>> 69f89181934f556a4bb1a40116f30071e910fe35
